d37935fc2e851a1d1972d291d8b19f25149577fe
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-22 10:25:31 -0300

    Initial commit

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="dashboard">
+    <h2>Meu Dashboard</h2>
+    <p>Em breve, aqui teremos gráficos e resumos importantes do Finvy.</p>
+  </div>
+</template>
---
+++ frontend/src/views/ProductsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="products">
+    <h2>Gerenciamento de Produtos</h2>
+    <p>Aqui vamos listar, adicionar e editar nossos produtos.</p>
+  </div>
+</template>
