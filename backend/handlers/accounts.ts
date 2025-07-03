fbf56fda733bcc338b07de19453057cd3a6a1653
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-07-03 17:27:50 -0300

    Update accounts.ts

--- backend/handlers/accounts.ts
+++ backend/handlers/accounts.ts
@@ -16,10 +16,9 @@ export default async function handler(
   const userSupabase = getSupabaseClient(token);
   try {
     if (req.method === "GET") {
-      const { data, error: dbError } = await userSupabase // Usando o cliente com token do usuário
+      const { data, error: dbError } = await serviceRoleSupabase
         .from("accounts")
-        .select("*")
-        .eq("user_id", user_id);
+        .select("*");
       if (dbError) {
         console.error("Erro do Supabase ao buscar contas:", dbError);
         throw dbError;
