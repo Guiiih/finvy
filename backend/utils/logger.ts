8e382a2cc6772533f8b23dfc68df3a56d93174de
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-07-08 22:44:39 -0300

    Update logger.ts

--- backend/utils/logger.ts
+++ backend/utils/logger.ts
@@ -1,13 +1,13 @@
 import { pino } from 'pino';
 
 const logger = pino({
-  transport: {
+  transport: process.env.NODE_ENV !== 'production' ? {
     target: 'pino-pretty',
     options: {
       colorize: true,
       ignore: 'pid,hostname',
     },
-  },
+  } : undefined,
 });
 
 export default logger;
