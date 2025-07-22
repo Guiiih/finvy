d48010e6965b48eb45f3cc9ca0d15a6675055081
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-07-22 15:20:30 -0300

    .

--- backend/utils/corsHandler.ts
+++ backend/utils/corsHandler.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export function handleCors(req: VercelRequest, res: VercelResponse): boolean {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN as string);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS",
